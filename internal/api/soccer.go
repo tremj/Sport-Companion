9da10b11ae916f9c928796e9d4663ad4bf0beb0d
tremj <tremerjustin@gmail.com>
2024-04-18 22:44:09 -0400

    updated all needed endpoint request handles

--- internal/api/soccer.go
+++ internal/api/soccer.go
@@ -1,5 +1,38 @@
 package endpoints
 
-/*
-  API endpoints for Soccer
-*/
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func makeSoccerRequest(userSpec string) (string, bool) {
+	errorString := "Something went wrong."
+	URL := "https://v1.football.api-sports.io//" + userSpec
+	method := "GET"
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest(method, URL, nil)
+	if err != nil {
+		fmt.Println(err)
+		return errorString, false
+	}
+
+	req.Header.Add("x-rapidapi-key", "5c027afffa1013a9baac52513787e122")
+	req.Header.Add("x-rapidapi-host", URL)
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return errorString, false
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return errorString, false
+	}
+	return string(body), true
+}
